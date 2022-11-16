893bb5bd34fb65c08c4a71e4f2dcfec9ec0743e2
chen <2021289671@qq.com>
2022-11-16 14:15:18 +0800

    User

--- src/main/java/com/example/config/Swagger2Config.java
+++ src/main/java/com/example/config/Swagger2Config.java
@@ -11,16 +11,15 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/*@Configuration
-@EnableSwagger2*/
+@Configuration
+@EnableSwagger2
 public class Swagger2Config {
-    //@Bean
+    @Bean
     public Docket buildDocket() {
         return  new Docket(DocumentationType.SWAGGER_2)
                 .enable(true)
                 .apiInfo(apiInfo())//调用下面apiInfo()方法
                 .select()
-                //Controller所在路径
                 .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
                 .paths(PathSelectors.any())
                 .build();
--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,6 +1,7 @@
 package com.example.domain;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +17,39 @@ public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty("用户主键id")
     private Long userId;
-    private String userUsername;
+
+    @ApiModelProperty("用户姓名")
+    private String username;
+
+    @ApiModelProperty("用户密码")
     private String userPassword;
+
+    @ApiModelProperty("用户专业类别")
     private String userMajorCategory;
+
+    @ApiModelProperty("用户职位")
     private String userPosition;
+
+    @ApiModelProperty("邮箱地址")
     private String userMail;
+
+    @ApiModelProperty("性别")
     private String userSex;
+
+    @ApiModelProperty("在职状态，0离职，1在职")
     private String userStatus;
+
+    @ApiModelProperty("创建时间")
     private Date userCreateTime;
+
+    @ApiModelProperty("修改时间")
     private Date userUpdateTime;
+
+    @ApiModelProperty("修改人主键id")
     private Long userUpdatePerson;
+
+    @ApiModelProperty("最新登录信息")
     private Date userLoginTime;
 }
