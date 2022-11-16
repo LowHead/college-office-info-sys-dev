79b7274d716829589da89d25d3d0c50212a48bcc
chen <2021289671@qq.com>
2022-11-16 14:04:02 +0800

    User

--- src/main/java/com/example/CollegeOfficeInfoSysDevApplication.java
+++ src/main/java/com/example/CollegeOfficeInfoSysDevApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CollegeOfficeInfoSysDevApplication {
     public static void main(String[] args) {
         SpringApplication.run(CollegeOfficeInfoSysDevApplication.class);
--- src/main/java/com/example/domain/User.java
+++ src/main/java/com/example/domain/User.java
@@ -1,15 +1,21 @@
 package com.example.domain;
 
+import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+@ApiModel
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private Long userId;
     private String userUsername;
     private String userPassword;
