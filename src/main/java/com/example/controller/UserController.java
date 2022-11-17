ff9efb3f6f549ff333ddeb573a0f52015d516dd2
jiugan <85791414+jiugan@users.noreply.github.com>
2022-11-17 19:29:53 +0800

    11/16 init4

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -4,11 +4,16 @@ import com.example.common.Result;
 import com.example.common.SystemException;
 import com.example.domain.User;
 import com.example.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@Api("用户服务")
 public class UserController {
 
     @Autowired
@@ -22,6 +27,11 @@ public class UserController {
      * @throws SystemException
      */
     @GetMapping ("/login")
+    @ApiOperation("用户登录接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户名", required = true),
+            @ApiImplicitParam(name = "password", value = "密码", required = true),
+    })
     public Result login(@RequestParam String username, @RequestParam String password) throws SystemException {
         return userService.login(username,password);
     }
@@ -32,11 +42,18 @@ public class UserController {
      * @return
      */
     @PostMapping("/register")
+    @ApiOperation("用户注册接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "User", value = "JSON传值（User对象）", required = true),
+    })
     public Result register(@RequestBody User user){
         return userService.register(user);
     }
 
     @PutMapping("/update")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "User", value = "JSON传值（User对象）", required = true),
+    })
     public Result update_user(@RequestBody User user){
         return userService.update_user(user);
     }
