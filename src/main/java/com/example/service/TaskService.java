fd2b928ce90e0a7b05ee8699c6a64a621157a95b
LowHead <1070839375@qq.com>
2022-12-04 22:35:33 +0800

    文件上传下载

--- src/main/java/com/example/service/TaskService.java
+++ src/main/java/com/example/service/TaskService.java
@@ -1,7 +1,12 @@
 package com.example.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.common.Result;
 import com.example.domain.Task;
 
 public interface TaskService extends IService<Task> {
+
+//    Result task_update(){
+//
+//    }
 }
